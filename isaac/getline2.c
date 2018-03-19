e43c7c4a47f902aaf7540ca0d7d0eb6d87f72cca
Lisa Olson <olson.lisa94@gmail.com>
2018-03-19 21:31:30 +0000

    updates getline2.c to first iteration of shell

--- isaac/getline2.c
+++ isaac/getline2.c
@@ -13,11 +13,22 @@ int main(void)
 	char *buffer = NULL;
 	size_t bufsize = 0;
 	size_t characters;
+	int status;
+	int fork_ret;
+	char *argv[];
 
-	write(STDOUT_FILENO, "$ ", 2);
-	characters = getline(&buffer, &bufsize, stdin);
-	write(1, buffer, bufsize);
-	free(buffer);
-
+	while (status != EOF)
+	{
+		write(STDOUT_FILENO, "$ ", 2);		
+		characters = getline(&buffer, &bufsize, stdin);
+		write(1, buffer, bufsize);
+		argv[0] = malloc(characters * sizeof(char));
+		argv[0] = buffer;	
+		fork_ret = fork();
+		execve(argv[0],argv, NULL);
+		free(argv[0]);
+		free(buffer);
+		wait(&status);
+	}
 	return (0);
 }
