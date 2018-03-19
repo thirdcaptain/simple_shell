3765b0c94692c5a6cf725632f9ed279cc9541773
Isaac Wong <isaac@thirdcaptain.com>
2018-03-19 00:27:47 +0000

    Updated for loop

--- fwe.c
+++ fwe.c
@@ -2,40 +2,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 int main(void)
 {
-	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
+	char *argv[] = {"/bin/ls", "-l", ".", NULL};
 	printf("Before execve\n");
 
-	pid_t pids[10];
 	int i;
 	int n = 5;
+	int status = 0, status1  = 0;
 
 /* Start children. */
 	for (i = 0; i < n; ++i) {
-  		if ((pids[i] = fork()) < 0) {
+	pid_t home = fork();
+  		if (home < 0) {
     			perror("fork");
     			exit(1);
-  		} else if (pids[i] == 0) {
+  		} else if (home == 0) {
     			/* execve on each child ? */
 			if (execve(argv[0], argv, NULL) == -1)
 			{
 				perror("Error");
 			}
 			printf("After execve\n");
-    			return (0);
   		}
+		else
+			wait(&status);
 	}
-
 /* Wait for children to exit. */
-	int status;
 	pid_t pid;
 	pid_t ppid = getppid();
 	while (n > 0) {
-  		pid = wait(&status);
+//  		pid = wait(&status1);
   		printf("Child's pid is :  %ld, exited with status: 0x%x\n", (long)pid, status);
   		printf("Parent's ppid is : %ld\n", (long)ppid);
 		--n;
 	}
+	return (0);
 }
