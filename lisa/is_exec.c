fced147c4765ab4b735528eeffa6e3019fb7069a
Lisa Olson <olson.lisa94@gmail.com>
2018-03-24 03:05:03 +0000

    returns each string with appended argument

---
+++ lisa/is_exec.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *append(char *path, char *name)
+{
+        int namelen = 0, pathlen = 0, i = 0, j = 0, k = 0, lengths = 0;
+        char *tmp;
+
+        while (name[namelen] != '\0')
+                namelen++;
+
+                i = 0;
+                j = 0;
+                pathlen = 0;
+
+                while (path[pathlen] != '\0')
+                {
+                        pathlen++;
+                }
+                lengths = pathlen + namelen;
+
+                tmp = malloc(sizeof(char) * lengths + 2);
+                if (tmp == NULL)
+                        return (NULL);
+
+                for(i = 0; i < pathlen; i++)
+                {
+                        tmp[i] = path[i];
+                }
+                tmp[i] = '/';
+                i++;
+                while (name[k] != '\0')
+                {
+                        tmp[i] = name[k];
+                        k++;
+                        i++;
+                }
+                tmp[i + 1] = '\0';
+
+        return (tmp);
+}
+
+
+char *is_exec(char **path, char *name)
+{
+	char *str;
+
+	while (*path)
+	{
+		str = append(*path, name);
+		printf("%s\n", str);
+		path++;
+	}
+	return (str);
+}
+
+int main(void)
+{
+	char *path[] = {"/bin", "/tmp", NULL};
+	char *name = "ls";
+	char *str;
+
+	str = is_exec(path, name);
+	printf("%s\n", str);
+}
